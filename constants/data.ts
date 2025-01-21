05046180d5ef5b31854619a4f13b62363061bf04
Arindam200 <arindammajumder2020@gmail.com>
2025-01-21 10:52:28 +0530

    add: improved images

--- constants/data.ts
+++ constants/data.ts
@@ -8,31 +8,31 @@ import {
 export const artworkData = [
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-6f33655f-a035-451d-870b-fe7d507846dc_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-46d27437-4f6a-4024-b403-c8e68259712d_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-bce43316-1990-41c8-934e-9b0820a17018_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-705be660-c519-4597-bbaf-67fe8b57ad7e_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-168b49a8-8289-40e6-8bb6-ec4633b6efe1_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-85208cd1-c392-4343-aa10-cbb1d097a6f3_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-275ea438-7f74-4f79-8b36-2cefb6ec0e06_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-44757940-22a5-465e-afaa-597e747b8459_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-5647e2b7-419f-4451-a90b-08598f136811_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-f8766dd9-edc9-478c-bb43-559d8b2b1103_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-2d9fd007-44b8-4efe-9eee-debf4a4aa13a_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-748e02b1-e5e6-4408-af80-f34b0daf71fb_00001_.webp",
   },
   {
     imageUrl:
-      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-3dbe853f-f374-4087-9ed6-eb98a0f48de1_00001_.webp",
+      "https://pictures-storage.storage.eu-north1.nebius.cloud/text2img-97306ea3-e10d-410b-83f0-e7fdb5148063_00001_.webp",
   },
 ];
 
